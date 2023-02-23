b0a18374b5ca470edd7a588797499a9886803ab1
Jeremy Tammik <jeremy.tammik@autodesk.com>
2023-02-23 23:12:06 +0100

    added YA_FSM by Tolentino Cotesta

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,6 @@ void loop() {
     {
       dropLoads(true);
     }
-
     else
     {
       dropLoads(false);
@@ -136,7 +135,6 @@ void loop() {
         voltageIsBelowThreshold = false;
       }
     }
-
     else
     {
       voltageIsBelowThreshold = true;
@@ -286,4 +284,101 @@ int main(int argc, char *argv[]) {
 
     return EXIT_SUCCESS;
 }
+
+// https://www.norwegiancreations.com/2017/03/state-machines-and-arduino-implementation/
+
+enum State_enum {STOP, FORWARD, ROTATE_RIGHT, ROTATE_LEFT};
+enum Sensors_enum {NONE, SENSOR_RIGHT, SENSOR_LEFT, BOTH};
+ 
+void state_machine_run(uint8_t sensors);
+void motors_stop();
+void motors_forward();
+void motors_right();
+void motors_left();
+uint8_t read_IR();
+ 
+uint8_t state = STOP;
+ 
+void setup(){
+}
+ 
+void loop(){
+  state_machine_run(read_IR());
+ 
+  delay(10);
+}
+ 
+void state_machine_run(uint8_t sensors) 
+{
+  switch(state)
+  {
+    case STOP:
+      if(sensors == NONE){
+        motors_forward();
+        state = FORWARD;
+      }
+      else if(sensors == SENSOR_RIGHT){
+        motors_left();
+        state = ROTATE_LEFT;
+      }
+      else{
+        motors_right();
+        state = ROTATE_RIGHT;
+      }
+      break;
+       
+    case FORWARD:
+      if(sensors != NONE){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_RIGHT:
+      if(sensors == NONE || sensors == SENSOR_RIGHT){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_LEFT:
+      if(sensors != SENSOR_RIGHT)
+      {
+        motors_stop();
+        state = STOP; 
+      }
+      break;
+  }
+}
+ 
+void motors_stop()
+{
+  //code for stopping motors
+}
+ 
+void motors_forward()
+{
+  //code for driving forward  
+}
+ 
+void motors_right()
+{
+  //code for turning right
+}
+ 
+void motors_left()
+{
+  //code for turning left
+}
+ 
+uint8_t read_IR()
+{
+  //code for reading both sensors
+}
+
+// https://github.com/bask185/ArduinoToolchain
+
+// https://www.hackster.io/tolentinocotesta/let-s-learn-how-to-use-finite-state-machine-with-arduino-c524ac
+// https://github.com/cotestatnt/YA_FSM
+
 */
