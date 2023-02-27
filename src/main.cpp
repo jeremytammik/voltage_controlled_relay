1a129fb2753088b056898f66b774a911adbf1e79
Allan Koech <allankoech.ke@gmail.com>
2023-02-27 13:49:19 +0300

    Minor edits

--- src/main.cpp
+++ src/main.cpp
@@ -17,73 +17,92 @@ float voltageTurnOffR2 = 26.5;
 YA_FSM stateMachine;
 
 // State Alias
-enum State { START, OFF, R1_ON, R2_ON };
+enum State
+{
+    START,
+    OFF,
+    R1_ON,
+    R2_ON
+};
 
 // Helper for print labels instead integer when state change
-const char * const stateName[] PROGMEM = { "START", "OFF", "R1_ON", "R2_ON" };
+const char *const stateName[] PROGMEM = {"START", "OFF", "R1_ON", "R2_ON"};
 
 // Minimum time to remain in each state except start
-#define MIN_TIME_MS 1000*60*1 // 1 minute (milliseconds * seconds * minutes) -> milliseconds
+#define MIN_TIME_MS 1000 * 60 * 1 // 1 minute (milliseconds * seconds * minutes) -> milliseconds
 
 // Output variables
 bool r1on = false;
 bool r2on = false;
 
 // Define "on entering" state machine callback function
-void onEnter() {
-  Serial.print(stateMachine.ActiveStateName());
-  Serial.println(F(" enter")); 
+void onEnter()
+{
+    Serial.print(stateMachine.ActiveStateName());
+    Serial.println(F(" enter"));
 }
 
 // Define "on leaving" state machine callback function
-void onExit() {
-  Serial.print(stateMachine.ActiveStateName());
-  Serial.println(F(" leave"));
+void onExit()
+{
+    Serial.print(stateMachine.ActiveStateName());
+    Serial.println(F(" leave"));
 }
 
 // Setup the State Machine
-void setupStateMachine() {
-  // Follow the order of defined enumeration for the state definition (will be used as index)
-  // Add States => name, timeout, onEnter cb, onState cb, onLeave cb
-  stateMachine.AddState(stateName[START], 0, 0, onEnter, nullptr, onExit);
-  stateMachine.AddState(stateName[OFF], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
-  stateMachine.AddState(stateName[R1_ON], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
-  stateMachine.AddState(stateName[R2_ON], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
+void setupStateMachine()
+{
+    // Follow the order of defined enumeration for the state definition (will be used as index)
+    // Add States => name, timeout, onEnter cb, onState cb, onLeave cb
+    stateMachine.AddState(stateName[START], 0, 0, onEnter, nullptr, onExit);
+    stateMachine.AddState(stateName[OFF], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
+    stateMachine.AddState(stateName[R1_ON], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
+    stateMachine.AddState(stateName[R2_ON], 0, MIN_TIME_MS, onEnter, nullptr, onExit);
 
-  stateMachine.AddAction(R1_ON, YA_FSM::N, r1on); // N -> while state is active led is ON
-  stateMachine.AddAction(R2_ON, YA_FSM::N, r2on);
+    stateMachine.AddAction(R1_ON, YA_FSM::N, r1on); // N -> while state is active led is ON
+    stateMachine.AddAction(R2_ON, YA_FSM::N, r2on);
 
-  // Add transitions with related trigger input callback functions
-  stateMachine.AddTransition(START, R1_ON, [](){return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR1;} );    
-  stateMachine.AddTransition(OFF, R1_ON, [](){return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR1;} );    
-  stateMachine.AddTransition(R1_ON, OFF, [](){return readVoltage(stateMachine.ActiveStateName()) < voltageTurnOffR1;} );    
-  stateMachine.AddTransition(R1_ON, R2_ON, [](){return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR2;} );    
-  stateMachine.AddTransition(R2_ON, R1_ON, [](){return readVoltage(stateMachine.ActiveStateName()) < voltageTurnOffR2;} );    
+    // Add transitions with related trigger input callback functions
+    stateMachine.AddTransition(START, R1_ON, []()
+                               { return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR1; });
+    stateMachine.AddTransition(OFF, R1_ON, []()
+                               { return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR1; });
+    stateMachine.AddTransition(R1_ON, OFF, []()
+                               { return readVoltage(stateMachine.ActiveStateName()) < voltageTurnOffR1; });
+    stateMachine.AddTransition(R1_ON, R2_ON, []()
+                               { return readVoltage(stateMachine.ActiveStateName()) > voltageTurnOnR2; });
+    stateMachine.AddTransition(R2_ON, R1_ON, []()
+                               { return readVoltage(stateMachine.ActiveStateName()) < voltageTurnOffR2; });
 }
 
-void setup() {
-  setupPins(); // set up Input/Output
-  Serial.begin(115200);
-  while(!Serial) {}  // Needed for native USB port only
-  Serial.println(F("Starting the Voltage Controlled Switch...\n"));
-  setupStateMachine();
-  dropLoads(); // turn off all loads
+void setup()
+{
+    setupPins(); // set up Input/Output
+    Serial.begin(115200);
+    while (!Serial)
+    {
+    } // Needed for native USB port only
+    Serial.println(F("Starting the Voltage Controlled Switch...\n"));
+    setupStateMachine();
+    dropLoads(); // turn off all loads
 }
 
-void loop() {
-  // Update State Machine
-  if(stateMachine.Update()){
-    Serial.print(F("Active state: "));
-    Serial.println(stateMachine.ActiveStateName());
-  }
+void loop()
+{
+    // Update State Machine
+    if (stateMachine.Update())
+    {
+        Serial.print(F("Active state: "));
+        Serial.println(stateMachine.ActiveStateName());
+    }
 
-  // Set outputs
-  digitalWrite(LOAD_1_LED, r1on);
-  digitalWrite(LOAD_1_RELAY, r1on);
-  digitalWrite(LOAD_2_LED, r2on);
-  digitalWrite(LOAD_2_RELAY, r2on);
+    // Set outputs
+    digitalWrite(LOAD_1_LED, r1on);
+    digitalWrite(LOAD_1_RELAY, r1on);
+    digitalWrite(LOAD_2_LED, r2on);
+    digitalWrite(LOAD_2_RELAY, r2on);
 
-  delay(500); // Sleep for half a second
+    delay(500); // Sleep for half a second
 }
 
 void setOn(int pin)
@@ -97,25 +116,25 @@ void setOff(int pin)
 }
 
 // Turn ON/OFF the loads
-void dropLoads(bool ok)
+void dropLoads()
 {
-        setOff(LOAD_1_RELAY);
-        setOff(LOAD_1_LED);
+    setOff(LOAD_1_RELAY);
+    setOff(LOAD_1_LED);
 
-  setOff(LOAD_2_RELAY);
-  setOff(LOAD_2_LED);
+    setOff(LOAD_2_RELAY);
+    setOff(LOAD_2_LED);
 
-  setOff(LOAD_3_RELAY);
-  setOff(LOAD_3_LED);
+    setOff(LOAD_3_RELAY);
+    setOff(LOAD_3_LED);
 
-  setOff(LOAD_4_RELAY);
-  setOff(LOAD_4_LED);
+    setOff(LOAD_4_RELAY);
+    setOff(LOAD_4_LED);
 
-  setOff(LOAD_5_RELAY);
-  setOff(LOAD_5_LED);
+    setOff(LOAD_5_RELAY);
+    setOff(LOAD_5_LED);
 
-  setOff(LOAD_6_RELAY);
-  setOff(LOAD_6_LED);
+    setOff(LOAD_6_RELAY);
+    setOff(LOAD_6_LED);
 }
 
 /*
